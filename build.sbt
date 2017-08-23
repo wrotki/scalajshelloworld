1413779c610e41a2e84cc23c3c8379d63de13ef0
mariusz <wrotki@msn.com>
2017-08-23 11:51:06 -0700

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+lazy val root = project.
+  enablePlugins(ScalaJSPlugin)
+
+scalaJSUseMainModuleInitializer := true
+
---
+++ src/main/scala/main.scala
@@ -0,0 +1,11 @@
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+    shakalaka
+  }
+  def shakalaka: Unit = {
+    println("Shakalaka!")
+  }
+}
