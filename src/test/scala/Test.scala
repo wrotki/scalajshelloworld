23d8413fcd1e74cfd7631819a13cc895e750143f
mariusz <wrotki@msn.com>
2017-08-25 13:35:59 -0700

    Disabled failing tests, added a pseudouseful one

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,18 +1,19 @@
 package test.utest.examples.examples
 
 import utest._
+import org.company.lib.Lib
 
 object HelloTests extends TestSuite{
   val tests = this{
     'test1{
-      throw new Exception("test1")
+      Lib.function()
     }
     'test2{
       1
     }
     'test3{
       val a = List[Byte](1, 2)
-      a(10)
+      //a(10)
     }
   }
 }
